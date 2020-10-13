a3ac73a1598bc76d9a023c49c68c1c1c22f8a0b1
JonasRace <jonas.r.eckert@gmail.com>
2020-10-13 15:31:58 -0500

    adding the piping command

--- makefile
+++ makefile
@@ -1,4 +1,3 @@
 shell: myshell.c
 	gcc myshell.c lex.yy.c -o shell -lfl
-clear:
-	clean
+
--- myshell.c
+++ myshell.c
@@ -34,11 +34,13 @@ void sig_handler(int signal) {
  */ 
 main() {
   int i;
-  char **args; 
+  char **args;
   int result;
   int block;
   int output;
   int input;
+	int pipes;
+	int lo_flag = 0;
   char *output_filename;
   char *input_filename;
 
@@ -57,10 +59,10 @@ main() {
   while(1) {
 
     // Print out the prompt and get the input
-    printf("->");
-    args = getaline();
+   	printf("->");
+   	args = getaline();
+
  
-   
     // No input, continue
     if(args[0] == NULL)
       continue;
@@ -68,12 +70,16 @@ main() {
     // Check for internal shell commands, such as exit
     if(internal_command(args))
       continue;
+		
+		//checking for number of pipe in command
+		pipes = (check_pipes(args));
 
     // Check for an ampersand
     block = (ampersand(args) == 0);
 
     // Check for redirected input
     input = redirect_input(args, &input_filename);
+
 
     switch(input) {
     case -1:
@@ -102,12 +108,43 @@ main() {
       break;
     }
     
-    do_command(args, block,input, input_filename, 
-	  output, output_filename); 
-   
+		if(pipes > 0){
+			do_pipes(args, pipes);
+		}
+		else{
+    	do_command(args, block,input, input_filename, 
+	  	output, output_filename);
 
+		}
   }
 }
+
+//checking for number of pipes in command
+int check_pipes(char **args){
+	int i;
+	int pipe_count = 0;
+
+	for(i = 1; args[i] != NULL; i++){
+		if(args[i][0] == '|'){
+			pipe_count++;
+		}
+	}
+	return pipe_count;
+}
+
+//trying to check for logical operators
+int lo_check(char **args){
+	int i;
+	for(i = 0; args[i] != NULL; i++){
+		if(args[i] == ';'){
+			return 1;		  
+				
+		}
+	}
+	return 0;
+	printf("%s\n", args[i]);
+	}
+
 
 
 /*
@@ -142,17 +179,110 @@ int internal_command(char **args) {
   return 0;
 }
 
+int do_pipes(char **args, int pipes) {
+    // The number of commands to run
+    const int commands = pipes + 1;
+    int i = 0;
+
+    int pipefds[2*pipes];
+
+    for(i = 0; i < pipes; i++){
+        if(pipe(pipefds + i*2) < 0) {
+            perror("Couldn't Pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int pid;
+    int status;
+
+    int j = 0;
+    int k = 0;
+    int s = 1;
+    int place;
+    int commandStarts[10];
+    commandStarts[0] = 0;
+
+    // This loop sets all of the pipes to NULL
+    // And creates an array of where the next
+    // Command starts
+
+    while (args[k] != NULL){
+        if(!strcmp(args[k], "|")){
+            args[k] = NULL;
+            // printf("args[%d] is now NULL", k);
+            commandStarts[s] = k+1;
+            s++;
+        }
+        k++;
+    }
+
+
+
+    for (i = 0; i < commands; ++i) {
+        // place is where in args the program should
+        // start running when it gets to the execution
+        // command
+        place = commandStarts[i];
+
+        pid = fork();
+        if(pid == 0) {
+            //if not last command
+            if(i < pipes){
+                if(dup2(pipefds[j + 1], 1) < 0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            //if not first command&& j!= 2*pipes
+            if(j != 0 ){
+                if(dup2(pipefds[j-2], 0) < 0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            int q;
+            for(q = 0; q < 2*pipes; q++){
+                    close(pipefds[q]);
+            }
+
+            // The commands are executed here,
+            // but it must be doing it a bit wrong
+            if( execvp(args[place], args) < 0 ){
+                    perror(*args);
+                    exit(EXIT_FAILURE);
+            }
+        }
+        else if(pid < 0){
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
+
+        j+=2;
+    }
+
+    for(i = 0; i < 2 * pipes; i++){
+        close(pipefds[i]);
+    }
+
+    for(i = 0; i < pipes + 1; i++){
+        wait(&status);
+    }
+}
+
+
+
 /* 
  * Do the command
  */
-int do_command(char **args, int block,
-	       int input, char *input_filename,
+int do_command(char **args, int block, int input, char *input_filename,
 	       int output, char *output_filename) {
   
   int result;
   pid_t child_id;
   int status;
-  int j;
 
   // Fork the child process
   child_id = fork();
@@ -166,6 +296,8 @@ int do_command(char **args, int block,
     perror("Error ENOMEM: ");
     return;
   }
+			
+
 
   if(child_id == 0) {
 		if(!block){ 
@@ -197,6 +329,10 @@ int do_command(char **args, int block,
   }
 
 }
+
+
+
+
 
 /*
  * Check for input redirection
